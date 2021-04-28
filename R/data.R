2ee4b3ef72b9f7e6f2692859cd91d09146ed83e7
Clemens Hug <clemens.hug@gmail.com>
2021-04-28 14:48:48 -0400

    Add AD example data

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' Gene expression data of Alzeihemer's disease patients
+#'
+#' A dataset containing log10-transformed gene expression counts of 293 genes
+#' from the brains of 259 patients with different stages of Alzheimer's disease.
+#'
+#' @format A data frame with 259 rows and 294 variables:
+#' \describe{
+#'   \item{Label}{Braak stage of Alzheimer's disease}
+#'   \item{...}{log10-transformed gene counts}
+#' }
+#' @source \url{https://adknowledgeportal.synapse.org/}
+"ad_data"
